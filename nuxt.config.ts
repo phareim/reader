9cf6e27d0393969815f2100acd815005a8f881a3
Petter Hareim <petter.hareim@miles.no>
2025-10-29 17:40:04 +0100

    new favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,8 @@ export default defineNuxtConfig({
         { name: 'description', content: 'Your friendly librarian for organizing and curating the web\'s knowledge' }
       ],
       link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.svg' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap' }
